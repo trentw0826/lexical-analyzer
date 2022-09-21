74800b9882a153300fe43e6f36133feedca01101
Trent Welling <trentw0826@gmail.com>
2022-09-21 12:22:01 -0600

    Update main.cpp to take input and pass to Lexer class as string

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include "Lexer.h"
 
 int main(int argc, char** argv) {
 
-//    Lexer* lexer = new Lexer();
+    Lexer* lexer = new Lexer();
 
-    std::cout << "Hello, World!\n";
+    //Verify command line has 2 arguments, an executable and .txt input
+    if (argc < 2){
+        std::cerr << "2 arguments required!\n";
+    }
 
-//    delete lexer;
+    //Verify that given .txt is usable
+    std::string inputText;
+    std::ifstream inputFile;
+    inputFile.open(argv[1]);
+    if (!inputFile) {
+        std::cerr << "Input file " << argv[1] << " could not be opened\n";
+        return 1;
+    }
 
+    //Convert .txt into an input string
+    std::stringstream bufferStream;
+    bufferStream << inputFile.rdbuf();
+    inputText = bufferStream.str();
+
+//    std::cout << "Input String: \"" << inputText << "\"\n";
+
+    lexer->Run(inputText);
+
+    delete lexer;
+
+    inputFile.close();
     return 0;
 }
