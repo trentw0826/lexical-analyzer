e3e8c7b5625c43b3b029bd6db29b0c0eb4f3fe0d
Trent Welling <trentw0826@gmail.com>
2022-09-21 12:56:01 -0600

    Update Token class to have 3 member variables, all possible token types, typeToString method, and toString

--- Token.cpp
+++ Token.cpp
@@ -1,5 +1,7 @@
 #include "Token.h"
 
 Token::Token(TokenType type, std::string description, int line) {
-    // TODO: initialize all member variables
+    this->type = type;
+    this->description = description;
+    this->line = line;
 }
--- Token.h
+++ Token.h
@@ -1,21 +1,87 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#include <iostream>
 #include <string>
 
 enum class TokenType {
     COLON,
     COLON_DASH,
-    // TODO: add the other types of tokens
-    UNDEFINED
+    COMMA,
+    PERIOD,
+    Q_MARK,
+    lEFT_PAREN,
+    RIGHT_PAREN,
+    MULTIPLY,
+    ADD,
+    SCHEMES,
+    FACTS,
+    RULES,
+    QUERIES,
+    ID,
+    STRING,
+    COMMENT,
+    UNDEFINED,
+    ENDOFFILE
 };
 
 class Token
 {
 private:
-    // TODO: add member variables for information needed by Token
+    TokenType type;
+    std::string description;
+    int line;
 
 public:
     Token(TokenType type, std::string description, int line);
+
+    std::string typeToString(TokenType type){
+        switch(type){
+            case TokenType::COLON:
+                return "COLON";
+            case TokenType::COLON_DASH:
+                return "COLON_DASH";
+            case TokenType::COMMA:
+                return "COMMA";
+            case TokenType::PERIOD:
+                return "PERIOD";
+            case TokenType::Q_MARK:
+                return "Q_MARK";
+            case TokenType::lEFT_PAREN:
+                return "lEFT_PAREN";
+            case TokenType::RIGHT_PAREN:
+                return "RIGHT_PAREN";
+            case TokenType::MULTIPLY:
+                return "MULTIPLY";
+            case TokenType::ADD:
+                return "ADD";
+            case TokenType::SCHEMES:
+                return "SCHEMES";
+            case TokenType::FACTS:
+                return "FACTS";
+            case TokenType::RULES:
+                return "RULES";
+            case TokenType::QUERIES:
+                return "QUERIES";
+            case TokenType::ID:
+                return "ID";
+            case TokenType::STRING:
+                return "STRING";
+            case TokenType::COMMENT:
+                return "COMMENT";
+            case TokenType::UNDEFINED:
+                return "UNDEFINED";
+            case TokenType::ENDOFFILE:
+                return "EOF";
+            default:
+                std::cerr << "Unknown Token Type\n";
+                exit(1);
+        }
+    }
+
+    std::string toString(){
+        std::string typeString = typeToString(type);
+        return ("(" + typeString + ",\"" + description + "\"," + line + ")";
+    }
 
     // TODO: add other needed methods
 };
